2a88013d9094ee46976fb3162bb9a6a6ab97afd4
Ihor Kram <93744884+1hkr4m@users.noreply.github.com>
2022-01-02 19:42:41 +0200

    Update usr-mng.sh

--- usr-mng.sh
+++ usr-mng.sh
@@ -1,10 +1,38 @@
-#!/bin/bash
-
-FILE=/etc/passwd
+#!bin/bash
 
 Main ()
 {
+FILE=/etc/passwd
 n=1
 
-for name in $(awk 'BEGIN(FS=":")
+for name in $(awk 'BEGIN{FS=":"}{print $1}' < "FILE")
+do
+  echo "User$n = $name"
+  groups $name
+  su_status()
+  home_dir_weight()
+  last_login()
+  let "n+=1"
+done
+exit 0
+}
+
+su_status()
+{
+var=$(sudo -l -U $name)
+if [ $? -eq 0]
+  echo("has sudo status")
+else
+  echo("no sudo")
+fi
+}
+
+home_dir_weight()
+{
+
+}
+
+last_login()
+{
+return last $name | head -n 1
 }
