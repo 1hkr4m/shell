935a55f6051fcd27d058d1608d6b467b98c6e1f6
Ihor Kram <iproute127@gmail.com>
2022-01-11 00:13:26 +0200

    git-bb modification

--- git-bb.sh
+++ git-bb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 bb_push() {
-    cp ~/Git/python_learn/* ~/BitBucket/python_learn
-    cd ~/BitBucket/python_learn
+    cp ~/Git/shell/* ~/BitBucket/shell
+    cd ~/BitBucket/shell
     sleep 2
     git add .
-    git commit -m "migrate"
+    git commit -m "update from git hub"
     git push origin
 }
 
