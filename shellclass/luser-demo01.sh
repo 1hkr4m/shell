e4b27da2bb2927503d1ca43d83ef53f24b0a5bf1
Ihor Kram <iproute127@gmail.com>
2022-01-18 02:30:11 +0200

    11 lesson video

--- shellclass/luser-demo01.sh
+++ shellclass/luser-demo01.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+echo 'Hello'
 
+WORD='script'
+
+echo "$WORD"
+
+echo '$WORD'
+
+echo "This is the $WORD"
+
+echo "This is the ${WORD}"
+
+echo "${WORD}ing is fun!"
+
+echo "$WORDing is fun!"
+
+ENDING='ed'
+
+echo "This is ${WORD}${ENDING}"
+
+ENDING='ing'
+
+echo "This is ${WORD}${ENDING}"
+
+ENDING='s'
+
+echo "You are going to write many $WORD$ENDING in this class!"
