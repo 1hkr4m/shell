838e7dc96ba9c43130a42cfcdf01e6bf96b3bf68
Ihor Kram <iproute127@gmail.com>
2022-01-20 10:20:43 +0200

    demo 8

--- shellclass/Project 3/luser-demo08.sh
+++ shellclass/Project 3/luser-demo08.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# This script demonstrates I/O redirection
+
+# Redirect STDOUT to a file
+FILE="/tmp/data-test"
+head -n1 /etc/passwd > ${FILE}
+cat /tmp/data-test
+
+# Redirect STDIN to a program
+read LINE < ${FILE}
+echo $LINE
+
