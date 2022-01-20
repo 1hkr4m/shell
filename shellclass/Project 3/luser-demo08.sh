5b669af5dc44ea64479a688a29fb74998c566c99
Ihor Kram <iproute127@gmail.com>
2022-01-20 11:36:51 +0200

    4654

--- shellclass/Project 3/luser-demo08.sh
+++ shellclass/Project 3/luser-demo08.sh
@@ -11,3 +11,28 @@ cat /tmp/data-test
 read LINE < ${FILE}
 echo $LINE
 
+# Redirect stdin to a program using FD 0
+
+read LINE 0< ${FILE}
+echo
+echo "${LINE}"
+
+# Redirect stdout to a file using FD 1, overwriting the file
+head -n3 /etc/passwd 1> ${FILE}
+echo
+echo "${FILE}"
+
+cat $FILE
+
+# Redirect stderr to a file using FD 2
+ERR_FILE="/tmp/data.err"
+head -n3 /etc/passwd/ /fakefile 2> ${ERR_FILE}
+
+# Redirect stdout and stderr ot a file
+head -n3 /etc/passwd /fakefike &> ${FILE}
+echo
+echo "${FILE}"
+
+# Redirect stdout and stderr through a pipe
+echo 
+head -n3 /etc/passwd 
