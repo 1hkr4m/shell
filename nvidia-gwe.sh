06392f8ef481f4fd5b57255f305fdf7196cb0ec1
Ihor Kram <iproute127@gmail.com>
2022-01-27 12:03:39 +0200

    work nvidia

--- nvidia-gwe.sh
+++ nvidia-gwe.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
 # Repo with source and readme
 #https://gitlab.com/leinardi/gwe
 
 # Run gwe for cron
-flatpak run com.leinardi.gwe
-sleep 10
-/usr/bin/psensor
-exit 0
+nohup flatpak run com.leinardi.gwe 2>/dev/null &
+
+
+
+
+#sleep 10
+#/usr/bin/psensor
+#exit 0
