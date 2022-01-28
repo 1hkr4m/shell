b93a35d64e2d3f1058138556985fdafb0fdd8431
Ihor Kram <iproute127@gmail.com>
2022-01-28 23:35:48 +0200

    some changes

--- guest-setup-debian.sh
+++ guest-setup-debian.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-# Basic setup for my VMs
-sudo apt -y install net-tools
-sudo apt -y install tree
-sudo apt -y install git
-sudo apt -y install konsole
-sudo apt -y install keepassxc
+# Main setup for my VMs
+main_apps() {
+sudo apt install net-tools
+sudo apt install tree
+sudo apt install git
+sudo apt install konsole
+sudo apt install keepassxc
+sudo apt install openssh-server
+}
 
 # VisualStudio Code
+dev_apps() {
+sudo apt-get update
 sudo apt install apt-transport-https
-sudo -y apt update
 sudo -y apt install code
-
-curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
-sudo -y apt install virtualbox
-sudo apt install ./vagrant_2.2.9_x86_64.deb
-vagrant --version
-sleep 5
-sudo -y apt install wine-stable+
-sudo -y apt install synaptic
-sudo -y apt install terminator
-sudo -y apt install doublecmd-gtk
-sudo -y apt-get update
 sudo -y apt-get install python3.6
-sudo -y apt install filezilla
+}
+
+# Virtualization soft
+vm_apps() {
+curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
+sudo apt install -y virtualbox
+sudo apt install -y ./vagrant_2.2.9_x86_64.deb
+sudo apt install -y wine-stable+
+}
+
+# Other soft
+other_apps() {
+sudo apt install synaptic
+sudo apt install terminator
+sudo apt install doublecmd-gtk
+sudo apt install filezilla
+}
