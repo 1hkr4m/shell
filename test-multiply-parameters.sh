b85167959a6bcac07e1acc668c28cd90a4ccf1de
Ihor Kram <iproute127@gmail.com>
2022-02-02 21:12:45 +0200

    just give execution rights to all files

---
+++ test-multiply-parameters.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+func() {
+    let sum=$1+$2+$3+$5
+    echo $sum >> /tmp/test.txt
+}
+
+#func $@
+tput setaf 2
+cat <<eof
+lsjfosdjfosjdfoisjdfo
+lsdjfosdijf
+dlfsjdofjs9843u5r9348u
+eof
+tput sgr0
